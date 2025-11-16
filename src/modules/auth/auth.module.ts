59ec13c7a89ddd8d46cea30229a43a57257f053e
David Faco <david.faco@bugaboostudio.com>
2025-11-16 18:50:07 -0300

    fix: change pass fix

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -5,10 +5,11 @@ import { JwtStrategy } from './strategies/supabaseAuthStrategy.strategy';
 import { UsersService } from 'src/common/user.service';
 import { PrismaService } from 'prisma/services/prisma.service';
 import { HttpModule, HttpService } from '@nestjs/axios';
+import { SupabaseRestService } from 'src/supabaseRestService/supabase-rest.service';
 
 @Module({
   imports: [HttpModule],
   controllers: [AuthController],
-  providers: [AuthService, JwtStrategy, UsersService, PrismaService, HttpService],
+  providers: [AuthService, JwtStrategy, UsersService, PrismaService, HttpService, SupabaseRestService],
 })
 export class AuthModule {}
