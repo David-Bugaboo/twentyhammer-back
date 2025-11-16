e49f8dc6dca3898a08c70f3f4d5ffc7175c8bc4f
David Facó <david.faco@bugaboostudio.com>
2025-11-16 06:37:33 -0300

    fix:n fix for no suapabase url

--- src/supabase/supabase.client.ts
+++ src/supabase/supabase.client.ts
@@ -2,6 +2,6 @@ import 'dotenv/config';
 import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
-  process.env.SUPABASE_URL!,
-  process.env.ANON_KEY!,
+  process.env.SUPABASE_URL || "http://127.0.0.1/54321" ,
+  process.env.ANON_KEY || "1234567890",
 );
