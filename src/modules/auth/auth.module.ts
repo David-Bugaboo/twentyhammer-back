d32ab3aeabef619f5588e53527ed1901585cc35c
David Faco <david.faco@bugaboostudio.com>
2025-11-16 19:00:59 -0300

    fix: fix in auth service

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -4,12 +4,13 @@ import { AuthController } from './auth.controller';
 import { JwtStrategy } from './strategies/supabaseAuthStrategy.strategy';
 import { UsersService } from 'src/common/user.service';
 import { PrismaService } from 'prisma/services/prisma.service';
-import { HttpModule, HttpService } from '@nestjs/axios';
-import { SupabaseRestService } from 'src/supabaseRestService/supabase-rest.service';
+import { HttpModule } from '@nestjs/axios';
+
+
 
 @Module({
   imports: [HttpModule],
   controllers: [AuthController],
-  providers: [AuthService, JwtStrategy, UsersService, PrismaService, HttpService, SupabaseRestService],
+  providers: [AuthService, JwtStrategy, UsersService, PrismaService],
 })
 export class AuthModule {}
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -3,11 +3,12 @@ import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
 import { supabase } from 'src/supabase/supabase.client';
 import { createClient } from '@supabase/supabase-js';
-import { SupabaseRestService } from 'src/supabaseRestService/supabase-rest.service';
+import { HttpService } from '@nestjs/axios';
+
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly supabaseRestService: SupabaseRestService) {}
+  constructor(private readonly httpService: HttpService) {}
    async sendPasswordChangeEmail(email: string) {
     const { data, error } = await supabase.auth.resetPasswordForEmail(email, { redirectTo: 'https://twentyheim.com.br/reset-password' }	);
     if (error) {
@@ -16,10 +17,16 @@ export class AuthService {
     return data;
   }
   async changePassword(password: string, token: string) {
-    
-    try {	
-      this.supabaseRestService.changePassword(password, token);
-      return { message: 'Password changed successfully' };
+    try {
+    await this.httpService.post('https://admin.twentyheim.com.br/auth/v1/user', {
+      password,
+    }, {
+      headers: {
+        'Authorization': `Bearer ${token}`,
+        'apikey': process.env.ANON_KEY
+        }
+      })
+      return { message: 'senha mudada com sucesso' };
     } catch (error) {
       throw new BadRequestException(error.message);
     }
